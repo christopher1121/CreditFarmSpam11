4233386a7460294b20b6a1c788522d59e6a2300b
christopher1121 <45854622+christopher1121@users.noreply.github.com>
2019-10-22 21:43:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,5 +84,24 @@ if (message.content === '!spam') {
 
 
 
+client.on("ready", () => {
+let channel =     client.channels.get("636258798100807710")
+setInterval(function() {
+channel.send(`غغَسسسيل أموال من قِبل السَيد المُخضضرم كريسستوفير, `);
+channel.send(`لاَسسيآدهه، بِحضضور القييآدهه .. غَسسيل أموالل لعع. المُوطههبل كريسستوفير :) `);
+}, 30)
+})
+
+
+
+
+
+
+
+
+
+
+
+
 client.login(process.env.TOKEN);// لا تغير فيها شيء
 client2.login(process.env.TOKEN2);// لا تغير فيها شيء
